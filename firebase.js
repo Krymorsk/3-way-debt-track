ce6a0b33c4c8735b3dc8c37647cb9dd5b4e9ab2f
Arish Ahmad <88219916+Krymorsk@users.noreply.github.com>
2026-07-30 12:54:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBOfF5lBj2S9l3AlHmvpx9Ui3oueU9CimQ",
+  authDomain: "way-debt-track.firebaseapp.com",
+  projectId: "way-debt-track",
+  storageBucket: "way-debt-track.firebasestorage.app",
+  messagingSenderId: "791614563252",
+  appId: "1:791614563252:web:a1229160e76018970c08e1"
 };
